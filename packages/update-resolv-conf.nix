8dcb339bb36b5a7173836e7b3b69777dd45e6a46
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-08-21 18:32:17 +0200

    Fix update-resolv-conf override

--- packages/update-resolv-conf.nix
+++ packages/update-resolv-conf.nix
@@ -1,8 +1,12 @@
 with import <nixpkgs> {};
 
 # nix-collect-garbage -d kept removing the script, so I made this workaround.
-update-resolv-conf.overrideAttrs (old: {
+
+let binPath = lib.makeBinPath [ coreutils openresolv systemd ];
+
+in update-resolv-conf.overrideAttrs (old: {
   installPhase = ''
     install -Dm555 update-resolv-conf.sh $out/bin/update-resolv-conf
+    wrapProgram $out/bin/update-resolv-conf --prefix PATH : ${binPath}
   '';
 })
