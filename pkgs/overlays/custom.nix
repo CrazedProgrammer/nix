ec80d7fbe57d751dcfd84a1fb05a9bd91ed27e65
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-01-05 17:44:53 +0100

    Add luakit for testing

--- pkgs/overlays/custom.nix
+++ pkgs/overlays/custom.nix
@@ -5,6 +5,7 @@ self: super:
   bobthefish = super.callPackage ../custom/bobthefish.nix {};
   ccemux = super.callPackage ../custom/ccemux.nix {};
   riko4 = super.callPackage ../custom/riko4.nix {};
+  luakit = super.callPackage ../custom/luakit.nix {};
   urn = super.callPackage ../custom/urn.nix { enableLuaJit = true; };
   thelounge = super.callPackage ../custom/thelounge.nix {};
   dotfiles-bin = super.callPackage ../custom/dotfiles-bin.nix {};
