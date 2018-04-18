749df955db6403efe869863d9e0c583367cf7946
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-04-18 09:23:05 +0200

    Use Linux 4.15 for the time being

--- home/kernel/linux.nix
+++ home/kernel/linux.nix
@@ -1,11 +1,14 @@
-{ stdenv, buildPackages, hostPlatform, fetchurl, perl, callPackage, linux_latest, ... } @ args:
+{ stdenv, buildPackages, hostPlatform, fetchurl, perl, callPackage, linux_4_15, ... } @ args:
 
-let buildLinux = attrs: callPackage ./generic.nix attrs; in
+let
+  buildLinux = attrs: callPackage ./generic.nix attrs;
+  linux = linux_4_15;
+in
 
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = linux_latest.version;
+  version = linux.version;
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
@@ -13,5 +16,5 @@ buildLinux (args // rec {
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
-  src = linux_latest.src;
+  src = linux.src;
 } // (args.argsOverride or {}))
