dcad45134df58f178868dc483dd23f29788d075f
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-04-18 13:23:29 +0200

    Add function that handles vim plugin building

--- pkgs/overrides/neovim.nix
+++ pkgs/overrides/neovim.nix
@@ -1,38 +1,38 @@
-{ neovim, vimPlugins, vimUtils, fetchFromGitHub, ... }:
+{ neovim, vimPlugins, vimUtils, fetchFromGitHub, lib, ... }:
 
 let
-  better-whitespace = vimUtils.buildVimPluginFrom2Nix rec {
-    name = "better-whitespace-${version}";
-    version = "2017-07-19";
-    src = fetchFromGitHub {
-      owner = "ntpeters";
-      repo = "vim-better-whitespace";
-      rev = "688837f23daa953a921a56c1780464d182577e6c";
-      sha256 = "18dbk5i945q5vmybccxa7qx2hdp751pgskxkrjwpxc5c32xr1crs";
-    };
-  };
+  customPluginData = [
+    ["ntpeters/better-whitespace" "2017-07-19"
+     "688837f23daa953a921a56c1780464d182577e6c" "18dbk5i945q5vmybccxa7qx2hdp751pgskxkrjwpxc5c32xr1crs"]
+    ["rhysd/vim-clang-format" "2018-02-01"
+     "8ff1660a1e9f856479fffe693743521f4f3068cb" "1g9vs6cg7irmwqa1lz6i7xbq50svykhvax12vx7cpf2bxs8jfp3n"]
+    ["drmikehenry/vim-headerguard" "2015-04-28"
+     "e53b37fa0772ffe2f30209f6109f5f2ae0fbf48f" "0aq6405p6m4wlgak0zzb7rz6fs5f4gbd2fq4fzy683wspg1k5lq0"]
+  ];
 
-  vim-clang-format = vimUtils.buildVimPluginFrom2Nix rec {
-    name = "vim-clang-format-${version}";
-    version = "2018-02-01";
-    src = fetchFromGitHub {
-      owner = "rhysd";
-      repo = "vim-clang-format";
-      rev = "8ff1660a1e9f856479fffe693743521f4f3068cb";
-      sha256 = "1g9vs6cg7irmwqa1lz6i7xbq50svykhvax12vx7cpf2bxs8jfp3n";
-    };
-  };
+  buildCustomPlugins = plugins: with lib; listToAttrs (map (plugin:
+    let
+      fullname = splitString "/" (elemAt plugin 0);
+      owner = elemAt fullname 0;
+      name = elemAt fullname 1;
+      version = elemAt plugin 1;
+      rev = elemAt plugin 2;
+      sha256 = elemAt plugin 3;
+    in
+    { name = name;
+      value = vimUtils.buildVimPluginFrom2Nix {
+        name = "${name}-${version}";
+        version = version;
+        src = fetchFromGitHub {
+          owner = owner;
+          repo = name;
+          rev = rev;
+          sha256 = sha256;
+        };
+      };
+    }) plugins);
 
-  vim-headerguard = vimUtils.buildVimPluginFrom2Nix rec {
-    name = "vim-headerguard-${version}";
-    version = "2015-04-28";
-    src = fetchFromGitHub {
-      owner = "drmikehenry";
-      repo = "vim-headerguard";
-      rev = "e53b37fa0772ffe2f30209f6109f5f2ae0fbf48f";
-      sha256 = "0aq6405p6m4wlgak0zzb7rz6fs5f4gbd2fq4fzy683wspg1k5lq0";
-    };
-  };
+  customPlugins = buildCustomPlugins customPluginData;
 in
 
 neovim.override {
@@ -40,7 +40,7 @@ neovim.override {
   configure = {
     customRC = builtins.readFile ../../dotfiles/init.vim;
 
-    packages.plugins = with vimPlugins; {
+    packages.plugins = with vimPlugins; with customPlugins; {
       start = [
         deoplete-nvim
         rainbow
