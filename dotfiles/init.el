31ea5634c1051020a9ee8de8d06b6dd1aaa1980b
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-04-28 00:42:11 +0200

    Improve emacs config

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -4,29 +4,49 @@
 
 (defun require-all (paths)
   (mapcar (lambda (path)
-	 (require path))
+            (require path))
        paths))
 
+; Packages that need to be required
 (require-all
  '(evil
    linum-relative
    fiplr))
 
 
+; GUI
 (setq custom-safe-themes t)
-(load-theme 'zerodark t)
+(load-theme 'base16-chalk t)
+(set-cursor-color "#c0c0c0")
+(set-face-foreground 'font-lock-comment-face "#808080")
+(set-face-background 'trailing-whitespace "#A01010")
+(tool-bar-mode -1)
 
+; Evil mode and relative line numbers
 (evil-mode t)
-
 (linum-relative-global-mode t)
 
+; Colourise and match parens
 (setq show-paren-delay 0)
 (show-paren-mode t)
-
 (add-hook 'find-file-hook 'rainbow-delimiters-mode)
 
+; No trailing whitespace
+(setq show-trailing-whitespace t)
+
+; Recent files
 (setq recentf-max-menu-items 25)
 (setq recentf-save-file (file-truename "~/.emacs-recentf"))
 (recentf-mode 1)
+
+; Tabs
+(setq-default indent-tabs-mode nil)
+(setq lisp-indent-offset 2)
+
+; Key bindings
 (global-set-key "\M-n" 'recentf-open-files)
 (global-set-key "\M-p" 'fiplr-find-file)
+
+; Auto-complete
+(ac-config-default)
+(add-hook 'find-file-hook 'auto-complete-mode)
--- pkgs/overrides/emacs.nix
+++ pkgs/overrides/emacs.nix
@@ -2,13 +2,24 @@
 
 emacsWithPackages
   (epkgs: (with epkgs.melpaStablePackages; [
-    magit          # ; Integrate git <C-x g>
-    zerodark-theme # ; Nicolas' theme
+    magit
     evil
     linum-relative
     fiplr
     rainbow-delimiters
     free-keys
+    base16-theme
+    nix-mode
+    rust-mode
+    auto-complete
+
+
+#   zerodark-theme
+#   atom-one-dark-theme
+#   darktooth-theme
+#   flatui-dark-theme
+#   gruber-darker-theme
+#   doom-themes
   ]) ++ (with epkgs.elpaPackages; [
   ]))
 
