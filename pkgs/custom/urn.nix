4454c7565b0e6829b054007272d51de79a9b0b1e
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-05-15 22:42:56 +0200

    Improve Urn derivation

--- pkgs/custom/urn.nix
+++ pkgs/custom/urn.nix
@@ -1,64 +1,62 @@
-{ nixpkgs ? import <nixpkgs> {}
-, enableLuaJit ? false
-, lua ? nixpkgs.pkgs.lua
-, luajit ? nixpkgs.pkgs.luajit
-, readline ? nixpkgs.pkgs.readline
-, fetchFromGitLab ? nixpkgs.fetchFromGitLab
+{ stdenv, fetchFromGitLab, buildEnv, makeWrapper, lua, luajit, readline
+, useLuaJit ? false
 , extraLibraries ? []
 }:
 
 let
-  inherit (nixpkgs) stdenv;
-  ourVersion = "0.7.0";
-
+  urnVersion = "0.7.0";
   # Build a sort of "union package" with all the native dependencies we
   # have: Lua (or LuaJIT), readline, etc. Then, we can depend on this
-  # and refer to ${runtime} instead of ${lua}, ${readline}, etc
-  runtime = nixpkgs.buildEnv {
-    name = "urn-rt-${ourVersion}";
+  # and refer to ${urn-rt} instead of ${lua}, ${readline}, etc.
+  urn-rt = buildEnv {
+    name = "urn-rt-${urnVersion}";
     ignoreCollisions = true;
-    paths = if enableLuaJit then
+    paths = if useLuaJit then
               [ luajit readline ]
             else
               [ lua ];
   };
-  dependencies = if extraLibraries == [] then "" else "-with-libraries";
+
+  inherit (stdenv.lib) optionalString concatMapStringsSep;
 in
-  stdenv.mkDerivation rec {
-    name = "urn-${ourVersion}${dependencies}";
-    version = ourVersion;
-    src = fetchFromGitLab {
-      owner = "urn";
-      repo = "urn";
-      rev = "b71fbaba3bbb1c6bd4164ab5e918efcdb1e3f176";
-      sha256 = "1kj69c3rfp503k9pyq0lvihj08abb1af4llmc4232z46g1hp0c20";
-    };
 
-    buildInputs = [ runtime nixpkgs.pkgs.makeWrapper ];
-    # any packages that depend on the compiler have a transitive
-    # dependency on the runtime support
-    propagatedBuildInputs = buildInputs;
+stdenv.mkDerivation rec {
+  name = "urn-${optionalString (extraLibraries != []) "with-libraries-"}${version}";
+  version = urnVersion;
 
-    makeFlags = ["-B"];
+  src = fetchFromGitLab {
+    owner = "urn";
+    repo = "urn";
+    rev = "b71fbaba3bbb1c6bd4164ab5e918efcdb1e3f176";
+    sha256 = "1kj69c3rfp503k9pyq0lvihj08abb1af4llmc4232z46g1hp0c20";
+  };
 
-    installPhase = ''
-    install -Dm755 bin/urn.lua $out/bin/urn
-    mkdir -p $out/lib/
+  buildInputs = [ makeWrapper ];
+  # Any packages that depend on the compiler have a transitive
+  # dependency on the Urn runtime support.
+  propagatedBuildInputs = [ urn-rt ];
+
+  makeFlags = ["-B"];
+
+  installPhase = ''
+    mkdir -p $out/bin $out/lib
+    install -m 0755 bin/urn.lua $out/bin/urn
     cp -r lib $out/lib/urn
     wrapProgram $out/bin/urn \
       --add-flags "-i $out/lib/urn --prelude $out/lib/urn/prelude.lisp" \
-      --add-flags "${nixpkgs.lib.concatMapStringsSep " " (x: "-i ${x.libraryPath}") extraLibraries}" \
-      --prefix PATH : ${runtime}/bin/ \
-      --prefix LD_LIBRARY_PATH : ${runtime}/lib/
-    '';
+      --add-flags "${concatMapStringsSep " " (x: "-i ${x.libraryPath}") extraLibraries}" \
+      --prefix PATH : ${urn-rt}/bin/ \
+      --prefix LD_LIBRARY_PATH : ${urn-rt}/lib/
+  '';
 
-    meta = with stdenv.lib; {
-      homepage = https://squiddev.github.io/urn;
-      description = "A lean lisp implementation for Lua";
-      license = licenses.bsd3;
-    };
-    passthru = {
-      # useful for getting a nix-shell
-      urn-rt = runtime;
-    };
-  }
+  meta = with stdenv.lib; {
+    homepage = https://urn-lang.com;
+    description = "A Lisp dialect with a focus on minimalism which compiles to Lua";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ CrazedProgrammer ];
+  };
+
+  passthru = {
+    inherit urn-rt;
+  };
+}
--- pkgs/overlays/custom.nix
+++ pkgs/overlays/custom.nix
@@ -6,7 +6,7 @@ self: super:
   ccemux = super.callPackage ../custom/ccemux.nix {};
   riko4 = super.callPackage ../custom/riko4.nix {};
   luakit = super.callPackage ../custom/luakit.nix {};
-  urn = super.callPackage ../custom/urn.nix { enableLuaJit = true; };
+  urn = super.callPackage ../custom/urn.nix { useLuaJit = true; };
   technic-launcher = super.callPackage ../custom/technic-launcher.nix {};
   thelounge = super.callPackage ../custom/thelounge.nix {};
   compsize = super.callPackage ../custom/compsize.nix {};
