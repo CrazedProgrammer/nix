d26f45900d857c9857364fd511e70f7ea752ee9b
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-08-09 00:38:59 +0200

    Convert kernel config to new structured format

--- modules/home/kernel/default.nix
+++ modules/home/kernel/default.nix
@@ -1,8 +1,25 @@
-{ recurseIntoAttrs, linuxPackagesFor, callPackage, kernelPatches, ... } @ pkgs:
-recurseIntoAttrs (linuxPackagesFor (callPackage ./linux.nix {
-  kernelPatches = with kernelPatches; [
-    bridge_stp_helper
-    modinst_arg_list_too_long
-    (import ./kernel-gcc-patch.nix pkgs)
-  ];
+{ stdenv, recurseIntoAttrs, linuxPackagesFor, linux_latest, ... } @ pkgs:
+
+let
+  linux = linux_latest;
+in
+
+recurseIntoAttrs (linuxPackagesFor (linux.override {
+  kernelPatches = linux.kernelPatches ++ [ (import ./kernel-gcc-patch.nix pkgs) ];
+    structuredExtraConfig = with import <nixpkgs/lib/kernel.nix> { inherit (stdenv) lib; version = linux.version; }; {
+      PREEMPT = yes;
+      KERNEL_XZ = no;
+      MODULE_COMPRESS = no;
+      MODULE_COMPRESS_XZ = null;
+      MNATIVE = yes;
+
+      LEDS_TRIGGER_DISK = yes;
+
+      # We need this module early on, so compile it into the kernel image.
+      BTRFS_FS = yes;
+
+      # Not using Nvidia cards, so don't compile the (expensive) modules.
+      FB_NVIDIA_I2C = no;
+      DRM_NOUVEAU = no;
+    };
 }))
