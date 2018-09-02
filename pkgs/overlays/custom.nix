a8e960e217608da8c6e2fc228e1ddce769a055f4
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-09-02 14:41:23 +0200

    Use ccemux and urn from nixos-unstable

--- pkgs/overlays/custom.nix
+++ pkgs/overlays/custom.nix
@@ -3,10 +3,8 @@ self: super:
 {
   # Custom packages
   bobthefish = super.callPackage ../custom/bobthefish.nix {};
-  ccemux = super.callPackage ../custom/ccemux.nix {};
   riko4 = super.callPackage ../custom/riko4.nix {};
   luakit = super.callPackage ../custom/luakit.nix {};
-  urn = super.callPackage ../custom/urn.nix { useLuaJit = true; };
   technic-launcher = super.callPackage ../custom/technic-launcher.nix {};
   thelounge = super.callPackage ../custom/thelounge.nix {};
   ls_extended = super.callPackage ../custom/ls_extended.nix {};
