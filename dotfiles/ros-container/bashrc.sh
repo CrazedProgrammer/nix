aebbf46f1c735c28496668a1bf8b21a4d3c4944c
CrazedProgrammer <crazedprogrammer@gmail.com>
2020-04-07 22:01:09 +0200

    Add Webots and MORSE simulators to ROS docker

--- dotfiles/ros-container/bashrc.sh
+++ dotfiles/ros-container/bashrc.sh
@@ -4,3 +4,4 @@ source /opt/ros/melodic/setup.bash
 [ -f ./devel/setup.bash ] && source ./devel/setup.bash
 alias catkut="catkin_make -DFranka_DIR:PATH=/root/libfranka/build -Dfreenect2_DIR=/root/freenect2/lib/cmake/freenect2"
 alias runsim="roslaunch sim_world world.launch"
+alias webots="/root/webots/webots"
