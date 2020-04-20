f3d8a02f08a1e9b46d503539c10b426bc57af19c
CrazedProgrammer <crazedprogrammer@gmail.com>
2020-04-20 15:14:00 +0200

    Add shortcuts in ROS Dockerfile

--- dotfiles/ros-container/bashrc.sh
+++ dotfiles/ros-container/bashrc.sh
@@ -3,5 +3,6 @@
 source /opt/ros/melodic/setup.bash
 [ -f ./devel/setup.bash ] && source ./devel/setup.bash
 alias catkut="catkin_make -DFranka_DIR:PATH=/opt/libfranka/build -Dfreenect2_DIR=/opt/freenect2/lib/cmake/freenect2"
-alias runsim="roslaunch sim_world world.launch"
+alias runsim="roslaunch lidarps_common lidarps.launch"
+alias dslam="roslaunch lidarps_ui debug_slam.launch"
 alias webots="/opt/webots/webots"
