ddd8ca2924a21b1e25fb314599624a9b14883088
CrazedProgrammer <crazedprogrammer@gmail.com>
2021-09-30 12:59:13 +0200

    Prevent cloud-sync.service from terminating in the middle of a sync

--- modules/home/cloud.nix
+++ modules/home/cloud.nix
@@ -15,6 +15,9 @@
       User = "casper";
       WorkingDirectory = "/home/casper/Cloud";
       ExecStart = "${pkgs.bitpocket}/bin/bitpocket sync";
+      # Wait for the process to exit instead of sending a SIGTERM signal
+      # on systemctl stop (the default). SIGCHLD is ignored by default.
+      KillSignal = "SIGCHLD";
     };
   };
 }
