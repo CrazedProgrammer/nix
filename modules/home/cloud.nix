f4a99eee2987d3c72877b4d4ed5476a2981bb953
CrazedProgrammer <crazedprogrammer@gmail.com>
2021-11-29 22:28:42 +0100

    Show notification in case cloud directory sync fails

--- modules/home/cloud.nix
+++ modules/home/cloud.nix
@@ -15,9 +15,13 @@
       User = "casper";
       WorkingDirectory = "/home/casper/Cloud";
       ExecStart = "${pkgs.bitpocket}/bin/bitpocket sync";
+      # Send notification in case sync fails.
+      ExecStopPost = "${pkgs.bash}/bin/sh -c '[ \"$EXIT_STATUS\" = 0 ] || ${pkgs.libnotify}/bin/notify-send \"Failed to sync cloud directory. Check the cloud-sync.service logs for more info.\"'";
       # Wait for the process to exit instead of sending a SIGTERM signal
       # on systemctl stop (the default). SIGCHLD is ignored by default.
       KillSignal = "SIGCHLD";
     };
+    # Required for notify-send to work.
+    environment.DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
   };
 }
